be803f9165a46a5ad6425a67cd2b79513b2f537d
choi <cejewe@naver.com>
2024-09-29 12:30:42 +0900

    로그인 페이지 디자인 수정

--- src/components/bar/sidebar.tsx
+++ src/components/bar/sidebar.tsx
@@ -2,15 +2,23 @@ import Link from "next/link";
 
 import { SidebarItem } from "./sidebar-item";
 import { SettingButton } from "./setting-button";
+import Image from "next/image";
 
 export const Sidebar = () => {
   return (
     <aside className="hidden h-full w-20 border-r border-black bg-black md:flex flex-col gap-y-4 items-center py-4 text-white">
       <Link href={"/"} className="text-2xl mb-6">
-        🦋
+        <Image
+          src={"/images/logo.svg"}
+          alt="logo"
+          width={30}
+          height={30}
+        />  
       </Link>
 
-      <SidebarItem />
+      <div className="flex flex-col items-center justify-center my-auto">
+        <SidebarItem />
+      </div>
 
       <div className="flex flex-col items-center justify-center mt-auto mb-2">
         <SettingButton />
--- src/features/auth/auth-card.tsx
+++ src/features/auth/auth-card.tsx
@@ -7,26 +7,37 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card";
+import Image from "next/image";
 
 export const AuthCard = () => {
   return (
-    <Card className="w-full h-full p-8 bg-black">
+    <Card className="w-full h-full p-8 bg-transparent backdrop-blur-md shadow-md border-white/50">
       <CardHeader className="px-0 pt-0">
-        <CardTitle className="text-center text-2xl text-white"> 
-            Untitled
+        <CardTitle className="flex items-center justify-center gap-x-4">
+          <Image
+            src={"/images/logo.svg"}
+            alt="logo"
+            width={30}
+            height={30}
+          />
+           <span className="text-white text-2xl font-bold">
+             Untitled
+          </span>
         </CardTitle>
       </CardHeader>
-      <CardContent className="space-y-5 px-0 pb-0">
+      <CardContent className="space-y-5 px-0 pb-0 pt-4">
         <div className="flex flex-col gap-y-2.5">
             <Button
                 disabled={false}
                 onClick={() => {}}
                 variant="outline"
                 size="lg"
-                className="w-full relative font-bold bg-white"
+                className="w-full relative bg-transparent flex items-center justify-start gap-x-14 hover:text-white hover:bg-black/20"
             >
-                <FcGoogle className="size-5 absolute top-2 left-2.5" />
+                <FcGoogle className="size-5" />
+                <span className="text-white font-bold text-sm">
                 Continue with Google
+                </span>
             </Button>
         </div>
       </CardContent>
