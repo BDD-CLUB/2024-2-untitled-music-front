23b3dc18701fe2056a7d76aee0d77b2fa88eafbe
choi <cejewe@naver.com>
2024-09-30 00:03:05 +0900

    로그인 api 테스트

--- src/features/auth/auth-card.tsx
+++ src/features/auth/auth-card.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import Image from "next/image";
 import { FcGoogle } from "react-icons/fc";
+import { useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -10,6 +13,14 @@ import {
 } from "@/components/ui/card";
 
 export const AuthCard = () => {
+  const router = useRouter();
+  
+  const googleOAuthUrl = process.env.NEXT_PUBLIC_GOOGLE_OAUTH_URL;
+
+  const handleGoogleLogin = async () => {
+    router.push(`${googleOAuthUrl}`)
+  }
+
   return (
     <Card className="w-full h-full p-8 bg-transparent backdrop-blur-md shadow-md border-white/50">
       <CardHeader className="px-0 pt-0">
@@ -29,7 +40,7 @@ export const AuthCard = () => {
         <div className="flex flex-col gap-y-2.5">
             <Button
                 disabled={false}
-                onClick={() => {}}
+                onClick={handleGoogleLogin}
                 variant="outline"
                 size="lg"
                 className="w-full relative bg-transparent flex items-center justify-start gap-x-14 hover:text-white hover:bg-white/10 md:hover:bg-black/20"
