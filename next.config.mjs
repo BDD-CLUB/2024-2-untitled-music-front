3472526dfad1601d36e406e86c682a25fbec44e4
choi <cejewe@naver.com>
2025-01-12 03:41:38 +0900

    성능 최적화 취소

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,9 @@ const nextConfig = {
   output: "standalone",
   images: {
     formats: ["image/avif", "image/webp"],
-    minimumCacheTTL: 31536000,
-    deviceSizes: [640, 750, 828, 1080, 1200],
-    imageSizes: [16, 32, 48, 64, 96],
+    minimumCacheTTL: 60,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     remotePatterns: [
       {
         protocol: "https",
@@ -43,10 +43,6 @@ const nextConfig = {
       ],
     });
     return config;
-  },
-  experimental: {
-    optimizeCss: true,
-    optimizePackageImports: ['@/components/ui'],
   },
 };
 
