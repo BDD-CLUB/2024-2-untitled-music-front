3f0410b3c18eefab7115eadbdf6505342f4531a0
choi <cejewe@naver.com>
2025-01-11 20:47:28 +0900

    vercel 오류 수정 3

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -18,8 +18,6 @@ type ToasterToast = ToastProps & {
   action?: ToastActionElement
 }
 
-type ToastActionType = "ADD_TOAST" | "UPDATE_TOAST" | "DISMISS_TOAST" | "REMOVE_TOAST"
-
 let count = 0
 
 function genId() {
