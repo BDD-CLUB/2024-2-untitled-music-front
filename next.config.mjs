71c97d89fd34ca2e36981e160e05351820f9d6f1
choi <cejewe@naver.com>
2025-01-23 23:30:48 +0900

    빌드 오류 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
     formats: ["image/avif", "image/webp"],
     minimumCacheTTL: 60 * 60 * 24 * 30,
